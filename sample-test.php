1490e26080630f08e68c0856bc326a93151029a5
Tony's Macbook <cstony0917@gmail.com>
2015-09-21 12:41:29 +0800

    rewrite with class

--- sample-test.php
+++ sample-test.php
@@ -4,19 +4,33 @@ include('../simple-php-unit-test/simple-php-unit-test.php');
 
 function a_function_to_be_test ( $input){
 	if($input === 'hello'){
-		return ture;
+		return true;
 	}else{
 		return false;
 	}
 }
 
+$spec = new SimplePHPUnitTest();
 
-describe('a_function_to_be_test input hello should be true', function(){
-	expect_true(a_function_to_be_test('hello'));
+
+$spec->describe('a_function_to_be_test input hello should be true', function(){
+	expect::true(a_function_to_be_test('hello'));
 	// expect_true(false);
 });
 
 
-describe('a_function_to_be_test input not hello should be false', function(){
-	expect_false(a_function_to_be_test('yppppppppp'));
+$spec->describe('a_function_to_be_test input not hello should be false', function(){
+	$r = a_function_to_be_test('yppppppppp');
+	expect::false($r);
 });
+
+$spec->describe('a_function_to_be_test should failed', function(){
+	$r = a_function_to_be_test('yppppppppp');
+	expect::true($r);
+});
+
+$spec->start();
+
+// $a = a_function_to_be_test('yppppppppp');
+
+// var_dump($a);
--- simple-php-unit-test.php
+++ simple-php-unit-test.php
@@ -5,44 +5,96 @@
 	GO READ THE README.md
 */
 
+class SimplePHPUnitTest {
+	private $specs = array();
 
-function expect_true( $input){
-	if( $input === true){
+	public function describe($desc, $callback){
+		$this->specs[] = array(
+			'desc' => $desc,
+			'callback' => $callback
+		);
+	}
 
-	}else{
-		$err = 'expect true but return "' . $input . '"';
-		echo "\n" . $err . "\n";
-		// echo "\n" . $err;
-		throw new Exception($err);
+	public function start(){
+		$fails = array();
+		foreach ($this->specs as $spec) {
+			$is_passed = true;
+			$callback = $spec['callback'];
+			$desc = $spec['desc'];
+
+
+			$err_msg = '';
+
+			try{
+				$callback();
+			} catch(Exception $e){
+				// var_dump($e);
+				$err_msg = $e->getMessage();
+				$is_passed = false;
+			}
+
+			if( $is_passed ){
+				// echo "\033[0;32m" . 'passed' . "\033[0m";
+				echo "\033[0;32m" . '.' . "\033[0m";
+			}else{
+				// $fails[] = $spec;
+				$fails[] = array(
+					'desc' => $desc,
+					'msg' => $err_msg
+				);
+
+				echo "\033[1;31m" . '.' . "\033[0m";
+			}
+		}
+
+
+		foreach ($fails as $fail) {
+			echo "\n";
+			echo "\033[1;31m";
+			echo $fail['desc'] . ", " . $fail['msg'];
+			// var_dump($fail);
+			echo "\033[0m";
+			echo "\n";
+		}
 	}
 }
 
-function expect_false( $input){
-	if( $input === true){
-		$err = 'expect false but return "' . $input . '"';
-		echo "\n" . $err . "\n";
-		// echo "\n" . $err;
-		throw new Exception($err);
+
+class expect{
+	public static function true($input){
+		// var_dump($input);
+		if( $input !== true){
+
+			if(gettype($input) === 'boolean'){
+				$input = ($input) ? 'true' : 'false';
+			}
+
+			$err = 'expect "true" but return "' . $input . '"';
+			// $err = 'expect "true" but return "false"';
+
+			// echo "-----------------\n";
+			// echo "\n" . $err . "\n";
+			// echo "\n-----------------";
+			// echo "\n" . $err;
+			throw new Exception($err);
+		}
 	}
-}
+	public static function false($input){
+		// var_dump($input !== false);
+		if( $input !== false){
 
-function describe($desc, $callback){
-	echo $desc . ' .............. ';
+			if(gettype($input) === 'boolean'){
+				$input = ($input) ? 'true' : 'false';
+			}
 
-	$is_passed = true;
+			$err = 'expect "false" but return "' . $input . '"';
+			// $err = 'expect "false" but return "true"';
 
-	try{
-		$callback();
-	} catch(Exception $e){
-		$is_passed = false;
+			// echo "\n-----------------\n";
+			// echo "\n" . $err . "\n";
+			// echo "\n-----------------\n";
+			// echo "\n" . $err;
+			throw new Exception($err);
+		}
 	}
-
-	if( $is_passed ){
-		echo "\033[0;32m" . 'passed' . "\033[0m";
-	}else{
-		echo "\033[1;31m" . 'falied' . "\033[0m";
-		echo "\n-----------------";
-	}
-
-	echo "\n";
 }
